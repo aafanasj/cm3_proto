3d8ae6a9202e7196d0530003cebcfb9272fe6844
aafanasj <aafanasj@yahoo.com>
2017-05-18 08:49:26 -0400

    Adding Button & Buzzer
    Adding Button & Buzzer

--- work/code/led/led.c
+++ work/code/led/led.c
@@ -4,6 +4,8 @@
 #define LEDRPin      4
 #define LEDGPin      5
 #define LEDBPin      6
+#define BuzzerPin    8
+#define ButtonPin    7
 
 int main(void )
 {
@@ -18,28 +20,40 @@ int main(void )
     pinMode(LEDGPin,OUTPUT);
     pinMode(LEDBPin,OUTPUT);
 
+    pinMode(BuzzerPin,OUTPUT);
+
+    pinMode(ButtonPin, INPUT);      // Set button as INPUT
+//    pullUpDnControl(ButtonPin, PUD_UP); // Enable pull-up resistor on button
+
     while(1)
     {
+        if(digitalRead(ButtonPin) == 0)
+        {
+            digitalWrite(BuzzerPin, LOW);
+            delay(100);
+            digitalWrite(BuzzerPin, HIGH);
+        }
+
         digitalWrite(LEDRPin, HIGH);
         digitalWrite(LEDGPin, HIGH);
         digitalWrite(LEDBPin, HIGH);
         digitalWrite(LEDRPin, LOW);
         printf("Led R On...\r\n");
-        delay(500);
+        delay(250);
 
         digitalWrite(LEDRPin, HIGH);
         digitalWrite(LEDGPin, HIGH);
         digitalWrite(LEDBPin, HIGH);
         digitalWrite(LEDGPin, LOW);
         printf("Led G On...\r\n");
-        delay(500);
+        delay(250);
 
         digitalWrite(LEDRPin, HIGH);
         digitalWrite(LEDGPin, HIGH);
         digitalWrite(LEDBPin, HIGH);
         digitalWrite(LEDBPin, LOW);
         printf("Led B On...\r\n");
-        delay(500);
+        delay(250);
 
         digitalWrite(LEDRPin, HIGH);
         digitalWrite(LEDGPin, HIGH);
@@ -48,7 +62,7 @@ int main(void )
         digitalWrite(LEDGPin, LOW);
         digitalWrite(LEDBPin, LOW);
         printf("Leds RGB On...\r\n");
-        delay(500);
+        delay(250);
     }
 
     return 0;
