a5a340b899ca891da347ba3b77b49a51249f6d8f
aafanasj <aafanasj@yahoo.com>
2017-05-18 01:00:37 -0400

    LED example
    LED example

---
+++ work/code/led/led.c
@@ -0,0 +1,31 @@
+#include <wiringPi.h>
+#include <stdio.h>
+
+#define LEDPin      0
+
+int main(void )
+{
+    if(wiringPiSetup() == -1)
+    {
+        printf("Setup wirinPi Failed !\r\n");
+
+        return 1;
+    }
+
+    pinMode(LEDPin,OUTPUT);
+
+    while(1)
+    {
+        digitalWrite(LEDPin, LOW);
+        printf("Led On...\r\n");
+        delay(500);
+
+        digitalWrite(LEDPin, HIGH);
+        printf("...Led Off\r\n");
+        delay(500);
+
+    }
+
+    return 0;
+}
+
