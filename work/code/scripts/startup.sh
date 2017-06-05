5d04c0b4d14fed030944abb643eb41de7016b120
aafanasj <aafanasj@yahoo.com>
2017-06-04 20:18:55 -0400

    Fix button
    Fix button

--- work/code/scripts/startup.sh
+++ work/code/scripts/startup.sh
@@ -4,6 +4,6 @@ echo "Start WIM services"
 
 sudo /home/pi/projects/cm3_proto/work/code/led/led &
 sudo /home/pi/projects/cm3_proto/work/code/beeper/beeper &
-sudo /home/pi/projects/cm3_proto/work/code/buttom/buttom &
+sudo /home/pi/projects/cm3_proto/work/code/button/button &
 sudo /home/pi/projects/cm3_proto/work/code/pir/pir &
 
