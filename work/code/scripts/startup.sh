37afd2ec12a20d8bb53e730a1d031cb1a7bea155
aafanasj <aafanasj@yahoo.com>
2017-06-04 20:05:21 -0400

    startup script update
    startup script update

--- work/code/scripts/startup.sh
+++ work/code/scripts/startup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "Hello World!"
+#echo "Hello World!"
+
+sudo /home/pi/projects/cm3_proto/work/code/led/led &
+sudo /home/pi/projects/cm3_proto/work/code/beeper/beeper &
+sudo /home/pi/projects/cm3_proto/work/code/buttom/buttom &
+sudo /home/pi/projects/cm3_proto/work/code/pir/pir &
+
